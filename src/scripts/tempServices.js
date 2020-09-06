c9acfa22c6406f30759b773195fcab60fba18a91
AhmedWahba <ahmed.wahba1990@gmail.com>
2020-09-07 01:19:57 +0300

    add styles - fonts - services - before deploy

---
+++ src/scripts/tempServices.js
@@ -0,0 +1,52 @@
+
+function getClientCity() {
+    return new Promise((resolve, reject) => {
+        var data = null;
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+    
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                resolve(JSON.parse(this.responseText));
+            } 
+        });
+        const URL = "https://api.ipregistry.co/?key=hyl7c0xgpb0yth";
+    
+        xhr.open("GET", URL, true);
+    
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Access-Control-Allow-Credentials", true);
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhr.send(data);
+    })
+}
+
+function getTempratureForCity(city) {
+    return new Promise((resolve, reject) => {
+        var data = null;
+
+        var xhr = new XMLHttpRequest();
+        xhr.withCredentials = false;
+    
+        xhr.addEventListener("readystatechange", function () {
+            if (this.readyState === 4) {
+                resolve(JSON.parse(this.responseText));
+            } 
+        });
+    
+        let params = {
+            q: city ? city : 'New York',
+            appid: 'd43a204d705dee337054cb4e8b38fdeb',
+            units: 'metric'
+        };
+        const URL = "http://api.openweathermap.org/data/2.5/weather?q=";
+    
+        xhr.open("GET", URL + params.q + "&appid=" + params.appid + "&units=" + params.units, true);
+    
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.setRequestHeader("Access-Control-Allow-Credentials", true);
+        xhr.setRequestHeader("Access-Control-Allow-Origin", "*");
+        xhr.send(data);
+    })
+}
